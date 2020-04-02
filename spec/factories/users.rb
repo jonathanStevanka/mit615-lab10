229faab456af6c3c0fe185a59ed93b29f1ce408e
jonathan stevanka <jonathanstevanka@yahoo.com>
2020-04-02 17:09:08 -0400

    fix for docker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :admin, class: User do
     sequence(:email) { |n| Faker::Internet.safe_email(name: "admin_user#{n}") }
     password { 'test1234' }
-    role 'Admin'
+    roles {'Admin'}
   end
 
   factory :user, class: User do
